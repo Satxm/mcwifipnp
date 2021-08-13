92a6bc7123f8754f56a1bc37c47ec1dc2682bedb
Satxm <56530136+Satxm@users.noreply.github.com>
2021-08-13 20:51:49 +0800

    Add files via upload

--- fabric/src/main/java/io/github/satxm/mcwifipnp/mixin/MixinPauseScreen.java
+++ fabric/src/main/java/io/github/satxm/mcwifipnp/mixin/MixinPauseScreen.java
@@ -11,8 +11,8 @@ import net.minecraft.client.gui.screens.Screen;
 
 @Mixin(PauseScreen.class)
 public class MixinPauseScreen {
-	@Dynamic("lambda in init")
-	@Redirect(method = "lambda$createPauseMenu$8" , at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V") , remap = false)
+	@Dynamic()
+	@Redirect(method = "method_19838" , at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"))
 	private void replaceOpenToLAN(Minecraft client, Screen toOpen) {
 		client.setScreen(new ShareToLanScreen((Screen) (Object) this));
 	}
