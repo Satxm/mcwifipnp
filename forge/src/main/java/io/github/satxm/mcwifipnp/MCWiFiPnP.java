b6b0a8a58de9e16d8abb03dcda4644558d7b8b2d
Satxm <56530136+Satxm@users.noreply.github.com>
2021-08-20 21:25:19 +0800

    1.4.5

--- forge/src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnP.java
+++ forge/src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnP.java
@@ -4,12 +4,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.HttpUtil;
-import net.minecraft.world.level.GameType;
-import net.minecraft.world.level.storage.LevelResource;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -30,17 +25,17 @@ public class MCWiFiPnP {
 	public void ChangeButton(GuiScreenEvent.InitGuiEvent.Post event) {
 		Minecraft client = Minecraft.getInstance();
 		Screen gui = event.getGui();
-		if (gui instanceof PauseScreen) {
-			Button ShareToLanNew = new Button(client.screen.width / 2 + 4, client.screen.height / 4 + 96 + -16, 98, 20, new TranslatableComponent("menu.shareToLan"), (button) -> {
-				client.setScreen(new ShareToLanScreen(gui));
-			});
-			ShareToLanNew.active = client.hasSingleplayerServer() && !client.getSingleplayerServer().isPublished();
-			Button ShareToLanOld = (Button) event.getWidgetList().get(6);
-			Button SaveAndExit = (Button) event.getWidgetList().get(7);
+		Button ShareToLanNew = new Button(client.screen.width / 2 + 4, client.screen.height / 4 + 96 + -16, 98, 20, new TranslatableComponent("menu.shareToLan"), (button) -> {
+			client.setScreen(new ShareToLanScreen(gui));
+		});
+		ShareToLanNew.active = client.hasSingleplayerServer() && !client.getSingleplayerServer().isPublished();
 
-			event.removeWidget(ShareToLanOld);
-			event.addWidget(ShareToLanNew);
-			event.addWidget(SaveAndExit);
+		if (gui instanceof PauseScreen) {
+			if(event.getWidgetList().size() == 8) {
+				event.removeWidget((Button) event.getWidgetList().get(6));
+				event.addWidget(ShareToLanNew);
+				event.addWidget((Button) event.getWidgetList().get(7));
+			}
         }
 	}
 
@@ -53,5 +48,4 @@ public class MCWiFiPnP {
 	public void onServerStopping(FMLServerStoppingEvent event){
 		MCWiFiPnPUnit.serverStopping(event.getServer());
 	}
-
 }
