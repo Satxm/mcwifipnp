11720293e5f87a90a3b4c8f3bd830a5d51d03066
Satxm <satxm@outlook.com>
2024-07-16 18:36:22 +0800

    1.19.2

--- src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnPUnit.java
+++ src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnPUnit.java
@@ -154,7 +154,7 @@ public class MCWiFiPnPUnit {
         Path location = server.getWorldPath(LevelResource.ROOT).resolve("mcwifipnp.json");
         MCWiFiPnPUnit.Config cfg;
         try {
-            cfg = gson.fromJson(new String(Files.readAllBytes(location)), MCWiFiPnPUnit.Config.class);
+            cfg = gson.fromJson(new String(Files.readAllBytes(location),"utf-8"), MCWiFiPnPUnit.Config.class);
             cfg.location = location;
         } catch (IOException | JsonParseException e) {
             try {
@@ -180,7 +180,7 @@ public class MCWiFiPnPUnit {
     static void saveConfig(Config cfg) {
         if (!cfg.needsDefaults) {
             try {
-                Files.write(cfg.location, toPrettyFormat(cfg).getBytes(), StandardOpenOption.TRUNCATE_EXISTING,
+                Files.write(cfg.location, toPrettyFormat(cfg).getBytes("utf-8"), StandardOpenOption.TRUNCATE_EXISTING,
                         StandardOpenOption.CREATE);
             } catch (IOException e) {
                 LOGGER.warn("Unable to write config file!", e);
@@ -190,9 +190,9 @@ public class MCWiFiPnPUnit {
 
     public static class Config {
         public int port = 25565;
-        public int maxPlayers = 10;
+        public int maxPlayers = 8;
         public String GameMode = "survival";
-        public String motd = "A Minecraft LAN World";
+        public String motd = Component.translatable("lanServer.title").getString();
         public boolean AllPlayersCheats = false;
         public boolean Whitelist = false;
         public boolean UseUPnP = true;
