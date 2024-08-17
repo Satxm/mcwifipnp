f873c5f45b21894025811a3419876a2abb23ae2a
Satxm <satxm@outlook.com>
2024-08-18 07:55:28 +0800

    1.20.2

--- fabric/src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnP.java
+++ fabric/src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnP.java
@@ -24,50 +24,50 @@ import net.minecraft.server.players.PlayerList;
 import io.github.satxm.mcwifipnp.mixin.PlayerListAccessor;
 
 public class MCWiFiPnP implements ModInitializer {
-    public static final String MODID = "mcwifipnp";
+  public static final String MODID = "mcwifipnp";
 
-    @Override
-    public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTING.register(this::onServerLoad);
-        ServerLifecycleEvents.SERVER_STOPPING.register(this::onServerStop);
-        ScreenEvents.AFTER_INIT.register(MCWiFiPnP::afterScreenInit);
+  @Override
+  public void onInitialize() {
+    ServerLifecycleEvents.SERVER_STARTING.register(this::onServerLoad);
+    ServerLifecycleEvents.SERVER_STOPPING.register(this::onServerStop);
+    ScreenEvents.AFTER_INIT.register(MCWiFiPnP::afterScreenInit);
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            DeOpCommands.register(dispatcher);
-            OpCommand.register(dispatcher);
-            WhitelistCommand.register(dispatcher);
-            BanIpCommands.register(dispatcher);
-            BanListCommands.register(dispatcher);
-            BanPlayerCommands.register(dispatcher);
-        });
-    }
+    CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+      DeOpCommands.register(dispatcher);
+      OpCommand.register(dispatcher);
+      WhitelistCommand.register(dispatcher);
+      BanIpCommands.register(dispatcher);
+      BanListCommands.register(dispatcher);
+      BanPlayerCommands.register(dispatcher);
+    });
+  }
 
-    public static void afterScreenInit(Minecraft client, Screen screen, int i, int j) {
-        if (screen instanceof PauseScreen) {
-            for (AbstractWidget button : Screens.getButtons(screen)) {
-                if (button.getMessage().equals(Component.translatable("menu.shareToLan"))) {
-                    Button newButton = Button.builder(Component.translatable("menu.shareToLan"), $ -> {
-                        client.setScreen(new ShareToLanScreenNew(screen));
-                    }).bounds(button.getX(), button.getY(), button.getWidth(), button.getHeight()).build();
-                    newButton.active = button.active;
-                    Screens.getButtons(screen).remove(button);
-                    Screens.getButtons(screen).add(newButton);
-                }
-            }
+  public static void afterScreenInit(Minecraft client, Screen screen, int i, int j) {
+    if (screen instanceof PauseScreen) {
+      for (AbstractWidget button : Screens.getButtons(screen)) {
+        if (button.getMessage().equals(Component.translatable("menu.shareToLan"))) {
+          Button newButton = Button.builder(Component.translatable("menu.shareToLan"), $ -> {
+            client.setScreen(new ShareToLanScreenNew(screen));
+          }).bounds(button.getX(), button.getY(), button.getWidth(), button.getHeight()).build();
+          newButton.active = button.active;
+          Screens.getButtons(screen).remove(button);
+          Screens.getButtons(screen).add(newButton);
         }
+      }
     }
+  }
 
-    private void onServerLoad(MinecraftServer server) {
-        MCWiFiPnPUnit.ReadingConfig(server);
-    }
+  private void onServerLoad(MinecraftServer server) {
+    MCWiFiPnPUnit.ReadingConfig(server);
+  }
 
-    private void onServerStop(MinecraftServer server) {
-        MCWiFiPnPUnit.CloseUPnPPort(server);
-    }
+  private void onServerStop(MinecraftServer server) {
+    MCWiFiPnPUnit.CloseUPnPPort(server);
+  }
 
-    public static void setMaxPlayers(IntegratedServer server, int num) {
-        PlayerList playerList = server.getPlayerList();
-        ((PlayerListAccessor) playerList).setMaxPlayers(num);
-    }
+  public static void setMaxPlayers(IntegratedServer server, int num) {
+    PlayerList playerList = server.getPlayerList();
+    ((PlayerListAccessor) playerList).setMaxPlayers(num);
+  }
 
 }
--- forge/src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnP.java
+++ forge/src/main/java/io/github/satxm/mcwifipnp/MCWiFiPnP.java
@@ -28,65 +28,65 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod(MCWiFiPnP.MODID)
 public class MCWiFiPnP {
-    public static final String MODID = "mcwifipnp";
+  public static final String MODID = "mcwifipnp";
 
-    public MCWiFiPnP() {
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
-        MinecraftForge.EVENT_BUS.addListener((final ScreenEvent.Init.Post evt) -> {
-            onAfterInitScreen(evt.getScreen().getMinecraft(), evt.getScreen(), evt.getListenersList(), evt::addListener, evt::removeListener);
+  public MCWiFiPnP() {
+    MinecraftForge.EVENT_BUS.register(this);
+    MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
+    MinecraftForge.EVENT_BUS.addListener((final ScreenEvent.Init.Post evt) -> {
+      onAfterInitScreen(evt.getScreen().getMinecraft(), evt.getScreen(), evt.getListenersList(), evt::addListener, evt::removeListener);
+    });
+  }
+
+  @SubscribeEvent
+  public void onServerStarting(ServerStartingEvent event) {
+    MCWiFiPnPUnit.ReadingConfig(event.getServer());
+  }
+
+  @SubscribeEvent
+  public void onRegisterCommands(RegisterCommandsEvent event) {
+    DeOpCommands.register(event.getDispatcher());
+    OpCommand.register(event.getDispatcher());
+    WhitelistCommand.register(event.getDispatcher());
+    BanIpCommands.register(event.getDispatcher());
+    BanListCommands.register(event.getDispatcher());
+    BanPlayerCommands.register(event.getDispatcher());
+  }
+
+  @SubscribeEvent
+  public void onServerStopping(ServerStoppingEvent event) {
+    MCWiFiPnPUnit.CloseUPnPPort(event.getServer());
+  }
+
+  public static void setMaxPlayers(IntegratedServer server, int num) {
+    PlayerList playerList = server.getPlayerList();
+    playerList.maxPlayers = num;
+  }
+
+  public static void onAfterInitScreen(Minecraft client, Screen screen, List<GuiEventListener> children,
+      Consumer<GuiEventListener> add, Consumer<GuiEventListener> remove) {
+    if (screen instanceof PauseScreen pauseScreen && screen.getClass() == PauseScreen.class) {
+      if (pauseScreen.showsPauseMenu()) {
+        findButton(children).ifPresent(button -> {
+          Button newButton = Button.builder(Component.translatable("menu.shareToLan"), $ -> {
+            client.setScreen(new ShareToLanScreenNew(screen));
+          }).bounds(button.getX(), button.getY(), button.getWidth(), button.getHeight()).build();
+          newButton.active = button.active;
+          remove.accept(button);
+          add.accept(newButton);
         });
+      }
     }
+  }
 
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        MCWiFiPnPUnit.ReadingConfig(event.getServer());
+  private static Optional<Button> findButton(List<GuiEventListener> widgets) {
+    for (GuiEventListener widget : widgets) {
+      if (widget instanceof Button button) {
+        if (button.getMessage().equals(Component.translatable("menu.shareToLan")))
+          return Optional.of(button);
+      }
     }
-
-    @SubscribeEvent
-    public void onRegisterCommands(RegisterCommandsEvent event) {
-        DeOpCommands.register(event.getDispatcher());
-        OpCommand.register(event.getDispatcher());
-        WhitelistCommand.register(event.getDispatcher());
-        BanIpCommands.register(event.getDispatcher());
-        BanListCommands.register(event.getDispatcher());
-        BanPlayerCommands.register(event.getDispatcher());
-    }
-
-    @SubscribeEvent
-    public void onServerStopping(ServerStoppingEvent event) {
-        MCWiFiPnPUnit.CloseUPnPPort(event.getServer());
-    }
-
-    public static void setMaxPlayers(IntegratedServer server, int num) {
-        PlayerList playerList = server.getPlayerList();
-        playerList.maxPlayers = num;
-    }
-
-    public static void onAfterInitScreen(Minecraft client, Screen screen, List<GuiEventListener> children,
-            Consumer<GuiEventListener> add, Consumer<GuiEventListener> remove) {
-        if (screen instanceof PauseScreen pauseScreen && screen.getClass() == PauseScreen.class) {
-            if (pauseScreen.showsPauseMenu()) {
-                findButton(children).ifPresent(button -> {
-                    Button newButton = Button.builder(Component.translatable("menu.shareToLan"), $ -> {
-                        client.setScreen(new ShareToLanScreenNew(screen));
-                    }).bounds(button.getX(), button.getY(), button.getWidth(), button.getHeight()).build();
-                    newButton.active = button.active;
-                    remove.accept(button);
-                    add.accept(newButton);
-                });
-            }
-        }
-    }
-
-    private static Optional<Button> findButton(List<GuiEventListener> widgets) {
-        for (GuiEventListener widget : widgets) {
-            if (widget instanceof Button button) {
-                if (button.getMessage().equals(Component.translatable("menu.shareToLan")))
-                    return Optional.of(button);
-            }
-        }
-        return Optional.empty();
-    }
+    return Optional.empty();
+  }
 
 }
