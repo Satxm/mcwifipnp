970d7049036b4044cde56e5b442160e59b5a3eff
Satxm <satxm@outlook.com>
2025-10-18 22:40:32 +0800

    Fix Crash on Save and Quit
    Update 1.9.5
    fixes #62

--- src/main/java/io/github/satxm/mcwifipnp/mixin/MixinIntegratedServer.java
+++ src/main/java/io/github/satxm/mcwifipnp/mixin/MixinIntegratedServer.java
@@ -3,12 +3,15 @@ package io.github.satxm.mcwifipnp.mixin;
 import io.github.satxm.mcwifipnp.Config;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.Serial;
 
 @Mixin(IntegratedServer.class)
 public abstract class MixinIntegratedServer {
@@ -19,8 +22,11 @@ public abstract class MixinIntegratedServer {
 
     @Inject(method = "getMaxPlayers", at = @At("HEAD"), cancellable = true, require = 1, allow = 1)
     private void setMaxPlayers(CallbackInfoReturnable<Integer> ci) {
-        int i = Config.read(this.minecraft.getSingleplayerServer()).maxPlayers;
+        MinecraftServer server = this.minecraft.getSingleplayerServer();
+        if (server != null) {
+        int i = Config.read(server).maxPlayers;
         ci.setReturnValue(i);
         ci.cancel();
+        }
     }
 }
